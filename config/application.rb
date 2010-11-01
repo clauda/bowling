08fca9f0f2c3f30ad9bd31926879206958f249c4
Claudia Farias <krawdyah@gmail.com>
2010-11-01 10:33:14 -0300

    substituindo jquery.wysiwyg pela gem ckeditor

--- config/application.rb
+++ config/application.rb
@@ -31,12 +31,17 @@ module Bowling
     config.i18n.default_locale = "pt-BR"
 
     # JavaScript files you want as :defaults (application.js is always included).
-    config.action_view.javascript_expansions[:defaults] = %w()
+    # config.action_view.javascript_expansions[:defaults] = %w()
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+    
+    # CKEditor
+    # http://github.com/galetahub/rails-ckeditor
+    config.autoload_paths += %W( #{config.root}/app/models/ckeditor )
+
   end
 end
